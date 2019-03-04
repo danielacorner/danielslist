b13935978f388bf8a7373948f492d93532ff19dd
dcorn068 <dcorn068@users.noreply.github.com>
2019-03-03 23:05:13 -0500

    Updates

---
+++ precache-manifest.f9581ed9dfec7a5d9b3bec9c64478bd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d855299298343c137c2f",
+    "url": "/danielslist/static/js/runtime~main.d8552992.js"
+  },
+  {
+    "revision": "b3ac2dd67ece5c19a099",
+    "url": "/danielslist/static/js/main.b3ac2dd6.chunk.js"
+  },
+  {
+    "revision": "30fc1c996d6be4b8ed27",
+    "url": "/danielslist/static/js/2.30fc1c99.chunk.js"
+  },
+  {
+    "revision": "b3ac2dd67ece5c19a099",
+    "url": "/danielslist/static/css/main.e0c7ff32.chunk.css"
+  },
+  {
+    "revision": "586e72c863361a757c0483577538b335",
+    "url": "/danielslist/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/danielslist/precache-manifest.4473e30ed3f0a5af632068943ba52b97.js"
+  "/danielslist/precache-manifest.f9581ed9dfec7a5d9b3bec9c64478bd6.js"
 );
 
 workbox.clientsClaim();
