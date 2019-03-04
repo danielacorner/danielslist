f9c542e8f18730bed4c4148483dada492197d06e
dcorn068 <dcorn068@users.noreply.github.com>
2019-03-03 20:37:58 -0500

    Updates

---
+++ precache-manifest.332091c345cecbbb115adf79ed3e7896.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d855299298343c137c2f",
+    "url": "/danielslist/static/js/runtime~main.d8552992.js"
+  },
+  {
+    "revision": "d8df91dbbc966b2fd754",
+    "url": "/danielslist/static/js/main.d8df91db.chunk.js"
+  },
+  {
+    "revision": "69fd6f4fca9084531ff8",
+    "url": "/danielslist/static/js/2.69fd6f4f.chunk.js"
+  },
+  {
+    "revision": "d8df91dbbc966b2fd754",
+    "url": "/danielslist/static/css/main.e0c7ff32.chunk.css"
+  },
+  {
+    "revision": "e50a73c8bf7edd361921cbdfff713a82",
+    "url": "/danielslist/index.html"
+  }
+];
